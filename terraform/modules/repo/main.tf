9c5313a1bfdfd7f770cf506684380e0629afa9f7
Erik Rasmussen <erasmussen@tractorzoom.com>
2026-09-08 09:14:23 -0500

    feat(terraform/modules/repo): add explicit default branch configuration
    Add `github_branch_default` resource to explicitly set the default
    branch to `main` and update the `default_branch` output to reference
    the new resource instead of the repository's default branch attribute.

--- terraform/modules/repo/main.tf
+++ terraform/modules/repo/main.tf
@@ -11,3 +11,8 @@ resource "github_repository" "this" {
   allow_rebase_merge = false
   allow_squash_merge = true
 }
+
+resource "github_branch_default" "this" {
+  repository = github_repository.this.name
+  branch     = "main"
+}
